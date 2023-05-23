e912c22cd8df8536c831a87d6ac4e055740f31d4
Xiang Ding <dingxiang987@gmail.com>
2023-05-23 11:49:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ easy_questions = [
 ]
 
 medium_questions = [
-    {"prompt": "What is the captial of China?\nA: China\nB: Nanjing\nC: Jiangxie\nAnswer:", "answer": "B"},
+    {"prompt": "What is the capital of China?\nA: China\nB: Nanjing\nC: Jiangxie\nAnswer:", "answer": "B"},
     {"prompt": "What is 2 times 3?\nA: 6\nB: 12\nAnswer:", "answer": "A"}
 ]
 
@@ -19,24 +19,40 @@ class User:
 class Teacher(User):
     def __init__(self, name):
         super().__init__(name)
-        self.students = []
+        self.students = {}
 
     def add_student(self, student):
-        self.students.append(student)
+        if student.name not in self.students:
+            self.students[student.name] = student
+        else:
+            print("Student already exists!")
 
     def vis_all(self):
-        scores = [student.score for student in self.students]
-        print(np.histogram(scores))
+        if self.students:
+            scores = [student.score for student in self.students.values()]
+            print(np.histogram(scores))
+        else:
+            print("No students to visualize!")
 
-    def vis_one(self, student):
-        print(np.histogram(student.score))
+    def vis_one(self, student_name):
+        if student_name in self.students:
+            print(np.histogram(self.students[student_name].score))
+        else:
+            print(f"Student {student_name} not found!")
 
     def change_problem(self, question_index, level, new_prompt, new_answer):
-        if level == "easy":
-            easy_questions[question_index] = {"prompt": new_prompt, "answer": new_answer}
+        if level.lower() == "easy":
+            if 0 <= question_index < len(easy_questions):
+                easy_questions[question_index] = {"prompt": new_prompt, "answer": new_answer}
+            else:
+                print("Invalid question index!")
+        elif level.lower() == "medium":
+            if 0 <= question_index < len(medium_questions):
+                medium_questions[question_index] = {"prompt": new_prompt, "answer": new_answer}
+            else:
+                print("Invalid question index!")
         else:
-            medium_questions[question_index] = {"prompt": new_prompt, "answer": new_answer}
-
+            print("Invalid level!")
 
 class Student(User):
     def __init__(self, name):
@@ -44,14 +60,10 @@ class Student(User):
         self.score = defaultdict(int)
 
     def practice(self, level):
-        if level == "easy":
-            questions = easy_questions
-        elif level == "medium":
-            questions = medium_questions
-        else:
+        questions = easy_questions if level == "easy" else medium_questions if level == "medium" else None
+        if questions is None:
             print("Invalid level!")
             return
-
         for question in questions:
             answer = input(question['prompt'])
             if answer.upper() == question['answer']:
@@ -60,14 +72,13 @@ class Student(User):
     def vis_self(self):
         print(np.histogram(list(self.score.values())))
 
-
 def main():
     # Instantiate a Teacher object
     teacher = Teacher("Prof. Snape")
 
     # Infinite loop until the user decides to quit
     while True:
-        user_type = input("Are you a teacher or student?: ").lower()
+        user_type = input("Are you a teacher or student? (or type 'quit' to exit): ").lower()
         if user_type == "teacher":
             while True:
                 option = input("What would you like to do? \nA: Visualize overall performance from the students "
@@ -75,34 +86,34 @@ def main():
                 if option == "A":
                     teacher.vis_all()
                 elif option == "B":
-                    question_index = int(input("Enter question index to change: "))
-                    level = input("Enter level (easy/medium) to change: ")
-                    new_prompt = input("Enter new prompt: ")
-                    new_answer = input("Enter new answer: ")
+                    question_index =
+                    question_index = int(input("Enter the question index: "))
+                    level = input("Enter level (easy/medium): ").lower()
+                    new_prompt = input("Enter the new prompt: ")
+                    new_answer = input("Enter the new answer: ").upper()
                     teacher.change_problem(question_index, level, new_prompt, new_answer)
                 elif option == "C":
-                    student_name = input("Enter student name to visualize: ")
-                    for student in teacher.students:
-                        if student.name == student_name:
-                            teacher.vis_one(student)
-                            break
+                    student_name = input("Enter the student's name: ")
+                    teacher.vis_one(student_name)
                 else:
                     print("Invalid option!")
 
-                cont = input
-                cont = input("Would you like to to continue?[Y/N]: ").upper()
+                cont = input("Would you like to continue?[Y/N]: ").upper()
                 if cont == "N":
                     break
 
         elif user_type == "student":
-            name = input("Enter your name: ")
-            student = Student(name)
-            teacher.add_student(student)
+            student_name = input("Enter your name: ")
+            if student_name not in teacher.students:
+                student = Student(student_name)
+                teacher.add_student(student)
+            else:
+                student = teacher.students[student_name]
 
             while True:
                 choices = input("What would you like to do? \nA: Practice \nB: See progress \nC: Visualize performance\nAnswer:").upper()
                 if choices == "A":
-                    level = input("Enter level (easy/medium): ")
+                    level = input("Enter level (easy/medium): ").lower()
                     student.practice(level)
                 elif choices == "B":
                     for level, score in student.score.items():
@@ -115,6 +126,9 @@ def main():
                 cont = input("Would you like to to continue?[Y/N]: ").upper()
                 if cont == "N":
                     break
+
+        elif user_type == "quit":
+            break
 
         else:
             print("That's not a valid response. Please enter 'student' or 'teacher'.")
